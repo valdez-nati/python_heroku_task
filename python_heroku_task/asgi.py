6c3be8966270c7a7d3c5b965c470154259d4fcaf
keikamiguchi <kei.kamiguchi517@gmail.com>
2022-02-01 16:15:34 +0900

    [update]プロジェクト名変更

--- python_heroku_task/asgi.py
+++ python_heroku_task/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for generic_app project.
+ASGI config for python_heroku_task project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'generic_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_heroku_task.settings')
 
 application = get_asgi_application()
--- python_heroku_task/wsgi.py
+++ python_heroku_task/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for generic_app project.
+WSGI config for python_heroku_task project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'generic_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_heroku_task.settings')
 
 application = get_wsgi_application()
